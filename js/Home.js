27dba72239b8af6673bfa4228b6b99f7cb65f785
marc <marcpayet974@gmail.com>
2021-08-19 04:15:14 +0200

    test

---
+++ js/Home.js
@@ -0,0 +1,26 @@
+const UrlTeddies = 'http://localhost:3000/api/teddies'
+
+fetch(UrlTeddies)
+    .then( data => data.json())
+    .then(jsonListteddies =>{
+        console.log(jsonListteddies);
+        for(let jsonteddies of jsonListteddies){
+            let Teddies = new teddies(jsonteddies);
+            document.querySelector('.container').innerHTML += `<div>
+                                                                    <a>
+                                                                        <div class="CardTeddy">
+                                                                        <h3>${Teddies.name}</h3>
+                                                                            <div class="CardHeader">
+                                                                            <img src="${Teddies.imageUrl}" alt="" class="imgTeddy">
+                                                                            </div>
+                                                                            <div class="CardBody">
+                                                                                <p class="CardDescription">${Teddies.description}</p>
+                                                                                <p class="CardPrice">${Teddies.price}€</p>
+                                                                            
+                                                                            </div>
+                                                                        </div>
+                                                                    </a>
+                                                                </div>`;
+
+        }
+    });
